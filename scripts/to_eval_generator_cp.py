08d0ab6120b23fc88e18576b096334fef29cc7cd
Cyril <cyril.grelier@outlook.fr>
2022-12-02 23:05:01 +0100

    tests dual 10h

--- scripts/to_eval_generator_cp.py
+++ scripts/to_eval_generator_cp.py
@@ -26,60 +26,60 @@ os.mkdir(f"{output_directory}")
 
 with open("to_eval_cp", "w", encoding="UTF8") as file:
     # primal original
-    for instance in instances:
-        file.write(
-            "minizinc --solver or-tools --time-limit 3600000 --parallel 0 --compiler-statistics --solver-statistics --no-intermediate --output-mode json "
-            '-D "WVCP_SEARCH_STRATEGY=VERTICES_GENERIC" '
-            '-D "WVCP_SEARCH_RESTART=RESTART_NONE" '
-            '-D "WVCP_SEARCH_VARIABLES_COLORS=WVCPSV(INPUT_ORDER)" '
-            '-D "WVCP_SEARCH_DOMAIN_COLORS=INDOMAIN_MIN" '
-            '-D "WVCP_SEARCH_VARIABLES_WEIGHTS=WVCPSV(INPUT_ORDER)" '
-            '-D "WVCP_SEARCH_DOMAIN_WEIGHTS=INDOMAIN_SPLIT" '
-            '-D "WVCP_SEARCH_VARIABLES_VERTICES=WVCPSV(FIRST_FAIL)" '
-            '-D "WVCP_SEARCH_DOMAIN_VERTICES=INDOMAIN_SPLIT" '
-            '-D "WVCP_B={UB_COLORS,UB_SCORE}" '
-            '-D "WVCP_M={M_SORT}" '
-            "-d core/default_ub_colors.dzn "
-            "-d core/default_ub_score.dzn "
-            "-d core/no_cliques.dzn "
-            "-m primal/primal_solve.mzn "
-            f"-d ../original_wvcp_dzn/{instance}.dzn "
-            f"> ../{output_directory}/primal_original_{instance}.json"
-            "\n"
-        )
-    # primal reduced
-    for instance in instances:
-        file.write(
-            "minizinc --solver or-tools --time-limit 3600000 --parallel 0 --compiler-statistics --solver-statistics --no-intermediate --output-mode json "
-            '-D "WVCP_SEARCH_STRATEGY=VERTICES_GENERIC" '
-            '-D "WVCP_SEARCH_RESTART=RESTART_NONE" '
-            '-D "WVCP_SEARCH_VARIABLES_COLORS=WVCPSV(INPUT_ORDER)" '
-            '-D "WVCP_SEARCH_DOMAIN_COLORS=INDOMAIN_MIN" '
-            '-D "WVCP_SEARCH_VARIABLES_WEIGHTS=WVCPSV(INPUT_ORDER)" '
-            '-D "WVCP_SEARCH_DOMAIN_WEIGHTS=INDOMAIN_SPLIT" '
-            '-D "WVCP_SEARCH_VARIABLES_VERTICES=WVCPSV(FIRST_FAIL)" '
-            '-D "WVCP_SEARCH_DOMAIN_VERTICES=INDOMAIN_SPLIT" '
-            '-D "WVCP_B={UB_COLORS,UB_SCORE}" '
-            '-D "WVCP_M={M_SORT}" '
-            "-d core/default_ub_colors.dzn "
-            "-d core/default_ub_score.dzn "
-            "-d core/no_cliques.dzn "
-            "-m primal/primal_solve.mzn "
-            f"-d ../reduced_wvcp_dzn/{instance}.dzn "
-            f"> ../{output_directory}/primal_reduced_{instance}.json"
-            "\n"
-        )
+    # for instance in instances:
+    #     file.write(
+    #         "minizinc --solver or-tools --time-limit 3600000 --parallel 0 --compiler-statistics --solver-statistics --no-intermediate --output-mode json --json-stream "
+    #         '-D "WVCP_SEARCH_STRATEGY=VERTICES_GENERIC" '
+    #         '-D "WVCP_SEARCH_RESTART=RESTART_NONE" '
+    #         '-D "WVCP_SEARCH_VARIABLES_COLORS=WVCPSV(INPUT_ORDER)" '
+    #         '-D "WVCP_SEARCH_DOMAIN_COLORS=INDOMAIN_MIN" '
+    #         '-D "WVCP_SEARCH_VARIABLES_WEIGHTS=WVCPSV(INPUT_ORDER)" '
+    #         '-D "WVCP_SEARCH_DOMAIN_WEIGHTS=INDOMAIN_SPLIT" '
+    #         '-D "WVCP_SEARCH_VARIABLES_VERTICES=WVCPSV(FIRST_FAIL)" '
+    #         '-D "WVCP_SEARCH_DOMAIN_VERTICES=INDOMAIN_SPLIT" '
+    #         '-D "WVCP_B={UB_COLORS,UB_SCORE}" '
+    #         '-D "WVCP_M={M_SORT}" '
+    #         "-d core/default_ub_colors.dzn "
+    #         "-d core/default_ub_score.dzn "
+    #         "-d core/no_cliques.dzn "
+    #         "-m primal/primal_solve.mzn "
+    #         f"-d ../original_wvcp_dzn/{instance}.dzn "
+    #         f"> ../{output_directory}/primal_original_{instance}.json"
+    #         "\n"
+    #     )
+    # # primal reduced
+    # for instance in instances:
+    #     file.write(
+    #         "minizinc --solver or-tools --time-limit 3600000 --parallel 0 --compiler-statistics --solver-statistics --no-intermediate --output-mode json "
+    #         '-D "WVCP_SEARCH_STRATEGY=VERTICES_GENERIC" '
+    #         '-D "WVCP_SEARCH_RESTART=RESTART_NONE" '
+    #         '-D "WVCP_SEARCH_VARIABLES_COLORS=WVCPSV(INPUT_ORDER)" '
+    #         '-D "WVCP_SEARCH_DOMAIN_COLORS=INDOMAIN_MIN" '
+    #         '-D "WVCP_SEARCH_VARIABLES_WEIGHTS=WVCPSV(INPUT_ORDER)" '
+    #         '-D "WVCP_SEARCH_DOMAIN_WEIGHTS=INDOMAIN_SPLIT" '
+    #         '-D "WVCP_SEARCH_VARIABLES_VERTICES=WVCPSV(FIRST_FAIL)" '
+    #         '-D "WVCP_SEARCH_DOMAIN_VERTICES=INDOMAIN_SPLIT" '
+    #         '-D "WVCP_B={UB_COLORS,UB_SCORE}" '
+    #         '-D "WVCP_M={M_SORT}" '
+    #         "-d core/default_ub_colors.dzn "
+    #         "-d core/default_ub_score.dzn "
+    #         "-d core/no_cliques.dzn "
+    #         "-m primal/primal_solve.mzn "
+    #         f"-d ../reduced_wvcp_dzn/{instance}.dzn "
+    #         f"> ../{output_directory}/primal_reduced_{instance}.json"
+    #         "\n"
+    #     )
 
     # dual original
     for instance in instances:
         file.write(
-            "minizinc --solver or-tools --time-limit 3600000 --parallel 0 --compiler-statistics --solver-statistics --no-intermediate --output-mode json "
+            "minizinc --solver or-tools --time-limit 36000000 --parallel 0 --compiler-statistics --solver-statistics --no-intermediate --output-mode json --json-stream "
             '-D "MWSSP_SEARCH_STRATEGY=ARCS_SPECIFIC" '
             '-D "MWSSP_SEARCH_RESTART=RESTART_NONE" '
             '-D "MWSSP_SEARCH_VARIABLES_ARCS=DESC_WEIGHT_TAIL" '
             '-D "MWSSP_SEARCH_DOMAIN_ARCS=INDOMAIN_MAX" '
             '-D "WVCP_B={UB_COLORS,UB_SCORE}" '
-            '-D "WVCP_M={M_SORT}" '
+            '-D "WVCP_M={I_SORT}" '
             "-d core/default_ub_colors.dzn "
             "-d core/default_ub_score.dzn "
             "-d core/no_cliques.dzn "
@@ -88,16 +88,17 @@ with open("to_eval_cp", "w", encoding="UTF8") as file:
             f"> ../{output_directory}/dual_original_{instance}.json"
             "\n"
         )
+        
     # dual reduced
     for instance in instances:
         file.write(
-            "minizinc --solver or-tools --time-limit 3600000 --parallel 0 --compiler-statistics --solver-statistics --no-intermediate --output-mode json "
+            "minizinc --solver or-tools --time-limit 36000000 --parallel 0 --compiler-statistics --solver-statistics --no-intermediate --output-mode json --json-stream "
             '-D "MWSSP_SEARCH_STRATEGY=ARCS_SPECIFIC" '
             '-D "MWSSP_SEARCH_RESTART=RESTART_NONE" '
             '-D "MWSSP_SEARCH_VARIABLES_ARCS=DESC_WEIGHT_TAIL" '
             '-D "MWSSP_SEARCH_DOMAIN_ARCS=INDOMAIN_MAX" '
             '-D "WVCP_B={UB_COLORS,UB_SCORE}" '
-            '-D "WVCP_M={M_SORT}" '
+            '-D "WVCP_M={I_SORT}" '
             "-d core/default_ub_colors.dzn "
             "-d core/default_ub_score.dzn "
             "-d core/no_cliques.dzn "
@@ -106,57 +107,57 @@ with open("to_eval_cp", "w", encoding="UTF8") as file:
             f"> ../{output_directory}/dual_reduced_{instance}.json"
             "\n"
         )
-    # joint original
-    for instance in instances:
-        file.write(
-            "minizinc --solver or-tools --time-limit 3600000 --parallel 0 --compiler-statistics --solver-statistics --no-intermediate --output-mode json "
-            '-D "MWSSP_WVCP_SEARCH_STRATEGY=MWSSP" '
-            '-D "WVCP_SEARCH_STRATEGY=VERTICES_GENERIC" '
-            '-D "WVCP_SEARCH_RESTART=RESTART_NONE" '
-            '-D "WVCP_SEARCH_VARIABLES_COLORS=WVCPSV(INPUT_ORDER)" '
-            '-D "WVCP_SEARCH_DOMAIN_COLORS=INDOMAIN_MIN" '
-            '-D "WVCP_SEARCH_VARIABLES_WEIGHTS=WVCPSV(INPUT_ORDER)" '
-            '-D "WVCP_SEARCH_DOMAIN_WEIGHTS=INDOMAIN_SPLIT" '
-            '-D "WVCP_SEARCH_VARIABLES_VERTICES=WVCPSV(FIRST_FAIL)" '
-            '-D "WVCP_SEARCH_DOMAIN_VERTICES=INDOMAIN_SPLIT" '
-            '-D "MWSSP_SEARCH_STRATEGY=ARCS_SPECIFIC" '
-            '-D "MWSSP_SEARCH_RESTART=RESTART_NONE" '
-            '-D "MWSSP_SEARCH_VARIABLES_ARCS=DESC_WEIGHT_TAIL" '
-            '-D "MWSSP_SEARCH_DOMAIN_ARCS=INDOMAIN_MAX" '
-            '-D "WVCP_B={UB_COLORS,UB_SCORE}" '
-            '-D "WVCP_M={M_SORT}" '
-            "-d core/default_ub_colors.dzn "
-            "-d core/default_ub_score.dzn "
-            "-d core/no_cliques.dzn "
-            "-m joint/joint_solve.mzn "
-            f"-d ../original_wvcp_dzn/{instance}.dzn "
-            f"> ../{output_directory}/primal_original_{instance}.json"
-            "\n"
-        )
-    # joint reduced
-    for instance in instances:
-        file.write(
-            "minizinc --solver or-tools --time-limit 3600000 --parallel 0 --compiler-statistics --solver-statistics --no-intermediate --output-mode json "
-            '-D "MWSSP_WVCP_SEARCH_STRATEGY=MWSSP" '
-            '-D "WVCP_SEARCH_STRATEGY=VERTICES_GENERIC" '
-            '-D "WVCP_SEARCH_RESTART=RESTART_NONE" '
-            '-D "WVCP_SEARCH_VARIABLES_COLORS=WVCPSV(INPUT_ORDER)" '
-            '-D "WVCP_SEARCH_DOMAIN_COLORS=INDOMAIN_MIN" '
-            '-D "WVCP_SEARCH_VARIABLES_WEIGHTS=WVCPSV(INPUT_ORDER)" '
-            '-D "WVCP_SEARCH_DOMAIN_WEIGHTS=INDOMAIN_SPLIT" '
-            '-D "WVCP_SEARCH_VARIABLES_VERTICES=WVCPSV(FIRST_FAIL)" '
-            '-D "WVCP_SEARCH_DOMAIN_VERTICES=INDOMAIN_SPLIT" '
-            '-D "MWSSP_SEARCH_STRATEGY=ARCS_SPECIFIC" '
-            '-D "MWSSP_SEARCH_RESTART=RESTART_NONE" '
-            '-D "MWSSP_SEARCH_VARIABLES_ARCS=DESC_WEIGHT_TAIL" '
-            '-D "MWSSP_SEARCH_DOMAIN_ARCS=INDOMAIN_MAX" '
-            '-D "WVCP_B={UB_COLORS,UB_SCORE}" '
-            '-D "WVCP_M={M_SORT}" '
-            "-d core/default_ub_colors.dzn "
-            "-d core/default_ub_score.dzn "
-            "-d core/no_cliques.dzn "
-            "-m joint/joint_solve.mzn "
-            f"-d ../reduced_wvcp_dzn/{instance}.dzn "
-            f"> ../{output_directory}/joint_reduced_{instance}.json"
-            "\n"
-        )
+    # # joint original
+    # for instance in instances:
+    #     file.write(
+    #         "minizinc --solver or-tools --time-limit 3600000 --parallel 0 --compiler-statistics --solver-statistics --no-intermediate --output-mode json "
+    #         '-D "MWSSP_WVCP_SEARCH_STRATEGY=MWSSP" '
+    #         '-D "WVCP_SEARCH_STRATEGY=VERTICES_GENERIC" '
+    #         '-D "WVCP_SEARCH_RESTART=RESTART_NONE" '
+    #         '-D "WVCP_SEARCH_VARIABLES_COLORS=WVCPSV(INPUT_ORDER)" '
+    #         '-D "WVCP_SEARCH_DOMAIN_COLORS=INDOMAIN_MIN" '
+    #         '-D "WVCP_SEARCH_VARIABLES_WEIGHTS=WVCPSV(INPUT_ORDER)" '
+    #         '-D "WVCP_SEARCH_DOMAIN_WEIGHTS=INDOMAIN_SPLIT" '
+    #         '-D "WVCP_SEARCH_VARIABLES_VERTICES=WVCPSV(FIRST_FAIL)" '
+    #         '-D "WVCP_SEARCH_DOMAIN_VERTICES=INDOMAIN_SPLIT" '
+    #         '-D "MWSSP_SEARCH_STRATEGY=ARCS_SPECIFIC" '
+    #         '-D "MWSSP_SEARCH_RESTART=RESTART_NONE" '
+    #         '-D "MWSSP_SEARCH_VARIABLES_ARCS=DESC_WEIGHT_TAIL" '
+    #         '-D "MWSSP_SEARCH_DOMAIN_ARCS=INDOMAIN_MAX" '
+    #         '-D "WVCP_B={UB_COLORS,UB_SCORE}" '
+    #         '-D "WVCP_M={M_SORT}" '
+    #         "-d core/default_ub_colors.dzn "
+    #         "-d core/default_ub_score.dzn "
+    #         "-d core/no_cliques.dzn "
+    #         "-m joint/joint_solve.mzn "
+    #         f"-d ../original_wvcp_dzn/{instance}.dzn "
+    #         f"> ../{output_directory}/primal_original_{instance}.json"
+    #         "\n"
+    #     )
+    # # joint reduced
+    # for instance in instances:
+    #     file.write(
+    #         "minizinc --solver or-tools --time-limit 3600000 --parallel 0 --compiler-statistics --solver-statistics --no-intermediate --output-mode json "
+    #         '-D "MWSSP_WVCP_SEARCH_STRATEGY=MWSSP" '
+    #         '-D "WVCP_SEARCH_STRATEGY=VERTICES_GENERIC" '
+    #         '-D "WVCP_SEARCH_RESTART=RESTART_NONE" '
+    #         '-D "WVCP_SEARCH_VARIABLES_COLORS=WVCPSV(INPUT_ORDER)" '
+    #         '-D "WVCP_SEARCH_DOMAIN_COLORS=INDOMAIN_MIN" '
+    #         '-D "WVCP_SEARCH_VARIABLES_WEIGHTS=WVCPSV(INPUT_ORDER)" '
+    #         '-D "WVCP_SEARCH_DOMAIN_WEIGHTS=INDOMAIN_SPLIT" '
+    #         '-D "WVCP_SEARCH_VARIABLES_VERTICES=WVCPSV(FIRST_FAIL)" '
+    #         '-D "WVCP_SEARCH_DOMAIN_VERTICES=INDOMAIN_SPLIT" '
+    #         '-D "MWSSP_SEARCH_STRATEGY=ARCS_SPECIFIC" '
+    #         '-D "MWSSP_SEARCH_RESTART=RESTART_NONE" '
+    #         '-D "MWSSP_SEARCH_VARIABLES_ARCS=DESC_WEIGHT_TAIL" '
+    #         '-D "MWSSP_SEARCH_DOMAIN_ARCS=INDOMAIN_MAX" '
+    #         '-D "WVCP_B={UB_COLORS,UB_SCORE}" '
+    #         '-D "WVCP_M={M_SORT}" '
+    #         "-d core/default_ub_colors.dzn "
+    #         "-d core/default_ub_score.dzn "
+    #         "-d core/no_cliques.dzn "
+    #         "-m joint/joint_solve.mzn "
+    #         f"-d ../reduced_wvcp_dzn/{instance}.dzn "
+    #         f"> ../{output_directory}/joint_reduced_{instance}.json"
+    #         "\n"
+    #     )
